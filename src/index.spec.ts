56e9bea50243638e1e35e79c84afc11cef5cf4a7
Rarayeku <stefan.schmidt.me@gmail.com>
2024-07-01 06:10:52 +0200

    update: updated tests to check new badges

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,10 +4,22 @@ import { User } from './types/user.interface';
 
 describe('getUsersBadge', () => {
 
+  it(`get Godlike`, async () => {
+    expect(await getUsersBadge(getUserMock(2000))).toEqual(Icon.BADGE_GODLIKE);
+    expect(await getUsersBadge(getUserMock(2001))).toEqual(Icon.BADGE_GODLIKE);
+    expect(await getUsersBadge(getUserMock(1000000))).toEqual(Icon.BADGE_GODLIKE);
+  });
+
+  it(`get Platinum`, async () => {
+    expect(await getUsersBadge(getUserMock(100))).toEqual(Icon.BADGE_PLATINUM);
+    expect(await getUsersBadge(getUserMock(1999))).toEqual(Icon.BADGE_PLATINUM);
+    expect(await getUsersBadge(getUserMock(150))).toEqual(Icon.BADGE_PLATINUM);
+  });
+
   it(`get Gold`, async () => {
-    expect(await getUsersBadge(getUserMock(100))).toEqual(Icon.BADGE_GOLD);
     expect(await getUsersBadge(getUserMock(50))).toEqual(Icon.BADGE_GOLD);
-    expect(await getUsersBadge(getUserMock(1000000))).toEqual(Icon.BADGE_GOLD);
+    expect(await getUsersBadge(getUserMock(99))).toEqual(Icon.BADGE_GOLD);
+    expect(await getUsersBadge(getUserMock(75))).toEqual(Icon.BADGE_GOLD);
   });
 
   it(`get Silver`, async () => {
@@ -22,10 +34,20 @@ describe('getUsersBadge', () => {
     expect(await getUsersBadge(getUserMock(10))).toEqual(Icon.BADGE_BRONZE);
   });
 
+  it(`get Starter`, async () => {
+    expect(await getUsersBadge(getUserMock(1))).toEqual(Icon.BADGE_STARTER);
+    expect(await getUsersBadge(getUserMock(4))).toEqual(Icon.BADGE_STARTER);
+    expect(await getUsersBadge(getUserMock(2))).toEqual(Icon.BADGE_STARTER);
+  });
+
   it(`get no Icon`, async () => {
-    expect(await getUsersBadge(getUserMock(4))).toEqual(Icon.DEFAULT);
-    expect(await getUsersBadge(getUserMock(-100))).toEqual(Icon.DEFAULT);
     expect(await getUsersBadge(getUserMock(0))).toEqual(Icon.DEFAULT);
+    expect(await getUsersBadge(getUserMock(0))).toEqual(Icon.DEFAULT);
+  });
+
+  it(`handles negative solutionCount`, async () => {
+    expect(await getUsersBadge(getUserMock(-1))).toEqual(Icon.BADGE_BADASS);
+    expect(await getUsersBadge(getUserMock(-10))).toEqual(Icon.BADGE_BADASS);
   });
 
 });
