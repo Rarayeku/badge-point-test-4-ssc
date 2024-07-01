2b4ad1baae77969699b970684bed072949119cc6
Rarayeku <stefan.schmidt.me@gmail.com>
2024-07-01 06:10:31 +0200

    update: added new badges to thresholds and check for values below 0

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
 import { User } from './types/user.interface';
 import { Icon } from './types/icon.enum';
 
-
 const badges: { threshold: number; icon: Icon }[] = [
+  { threshold: 2000, icon: Icon.BADGE_GODLIKE },
+  { threshold: 100, icon: Icon.BADGE_PLATINUM },
   { threshold: 50, icon: Icon.BADGE_GOLD },
   { threshold: 25, icon: Icon.BADGE_SILVER },
-  { threshold: 5, icon: Icon.BADGE_BRONZE }
+  { threshold: 5, icon: Icon.BADGE_BRONZE },
+  { threshold: 1, icon: Icon.BADGE_STARTER },
 ];
 
-// I decided to ditch the null and facilitate all returns in the enum, also introduced a badges configuration for easy adding of future badges.
-// Since null is not allowed in enums, this seemed smother.
-// In a bigger application I would check if this causes any problems/goes against commited code conventions.
+
 export const getUsersBadge = async (user: User): Promise<Icon> => {
+  if (user.solutionCount < 0) {
+    return Icon.BADGE_BADASS;
+  }
+
   for (const { threshold, icon } of badges) {
+
     if (user.solutionCount >= threshold) {
       return icon;
     }
