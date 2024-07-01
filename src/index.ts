cc72c203614f103bbbc019414399f17ad0530489
Rarayeku <stefan.schmidt.me@gmail.com>
2024-07-01 06:56:51 +0200

    feature: user badge data calculation, show user statistics in console

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { User } from './types/user.interface';
 import { Icon } from './types/icon.enum';
+import { getAllUser as getAllUsers } from './user-store';
 
 const badges: { threshold: number; icon: Icon }[] = [
   { threshold: 2000, icon: Icon.BADGE_GODLIKE },
@@ -15,9 +16,7 @@ export const getUsersBadge = async (user: User): Promise<Icon> => {
   if (user.solutionCount < 0) {
     return Icon.BADGE_BADASS;
   }
-
   for (const { threshold, icon } of badges) {
-
     if (user.solutionCount >= threshold) {
       return icon;
     }
@@ -25,8 +24,43 @@ export const getUsersBadge = async (user: User): Promise<Icon> => {
   return Icon.DEFAULT;
 };
 
-function calculateUsersStatistics() {
-  // todo
+async function calculateUsersStatistics() {
+  const users = await getAllUsers();
+  const badgeCounts: { [key in Icon]: number } = {
+    [Icon.BADGE_GODLIKE]: 0,
+    [Icon.BADGE_PLATINUM]: 0,
+    [Icon.BADGE_GOLD]: 0,
+    [Icon.BADGE_SILVER]: 0,
+    [Icon.BADGE_BRONZE]: 0,
+    [Icon.BADGE_STARTER]: 0,
+    [Icon.BADGE_BADASS]: 0,
+    [Icon.DEFAULT]: 0,
+  };
+
+  const getUserBadgePromises = users.map(async user => {
+    const badge = await getUsersBadge(user);
+    badgeCounts[badge]++;
+    return { ...user, badge };
+  });
+
+  const usersWithBadge = await Promise.all(getUserBadgePromises);
+  const usersTotal = users.length;
+  const solutionsTotal = users.reduce((sum, user) => sum + user.solutionCount, 0);
+  const averageSolutions = solutionsTotal / usersTotal;
+  const usersTopFive = usersWithBadge.sort((a, b) => b.solutionCount - a.solutionCount).slice(0, 5);
+  const badgeTopOne = Object.keys(badgeCounts).reduce((a, b) =>
+    badgeCounts[a as Icon] > badgeCounts[b as Icon] ? a : b
+  ) as Icon;
+
+  // Print statistics to console
+  console.log('Statistics:');
+  console.log(`1. Total number of users: ${usersTotal}`);
+  console.log(`2. Average solution count: ${averageSolutions.toFixed(2)}`);
+  console.log('3. Top 5 users:');
+  usersTopFive.forEach((user, index) => {
+    console.log(`${index + 1}. ${user.username} with ${user.solutionCount} solutions`);
+  });
+  console.log(`4. Most given badge: ${badgeTopOne}`);
 }
 
-calculateUsersStatistics();
+calculateUsersStatistics().catch(console.error);
